97c44c3caeb83c1c7ac3b4fd8b9079f39b68558d
korrupt <k0rrupt.dev@gmail.com>
2024-07-31 12:24:24 +0200

    added global validation pipe

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,14 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostgresFeatureModule } from "@ip-resolver/nest/postgres/feature";
 import { NestDeviceFeatureModule } from '@ip-resolver/nest/device/feature';
+import { APP_PIPE } from '@nestjs/core';
 
 
 @Module({
   imports: [PostgresFeatureModule, NestDeviceFeatureModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({ transform: true, whitelist: true })
+    },
+    AppService
+  ],
 })
 export class AppModule {}
